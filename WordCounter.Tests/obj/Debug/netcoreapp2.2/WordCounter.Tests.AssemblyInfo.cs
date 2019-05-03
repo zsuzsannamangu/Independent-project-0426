66df5e87b5d2ed5f6c3faaa117d42e836c678630
zsuzsannamangu <zsuzsannamangu@gmail.com>
2019-05-03 08:09:13 -0700

    initial folders for user interface logic

--- WordCounter.Tests/obj/Debug/netcoreapp2.2/WordCounter.Tests.AssemblyInfo.cs
+++ WordCounter.Tests/obj/Debug/netcoreapp2.2/WordCounter.Tests.AssemblyInfo.cs
@@ -7,10 +7,10 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("WordCounter")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("WordCounter.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("WordCounter")]
-[assembly: System.Reflection.AssemblyTitleAttribute("WordCounter")]
+[assembly: System.Reflection.AssemblyProductAttribute("WordCounter.Tests")]
+[assembly: System.Reflection.AssemblyTitleAttribute("WordCounter.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
